a460d87e0732829a3724155dfd24c90070e2c324
bingmani0922 <95996860+bingmani0922@users.noreply.github.com>
2024-10-16 07:46:03 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("내가 만든 앱")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "내가 만든 본문"
 )
